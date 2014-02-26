b9a5aa0d04d52f9e0d00d8156990d82210abe4ac
Adrian Testa-Avila <github@custom-anything.com>
2014-02-25 23:10:02 -0800

    Create ArraySet.php

---
+++ ArraySet.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * maintains an array-like collection of distinct scalar values.
+ * @author  Adrian Testa-Avila <adrian@ATapp.info>
+ * @license GPL v3
+ */
+class ArraySet extends ArrayObject{
+
+    public function __construct( array $items ){
+        parent::__construct( array_combine( $items,$items ) );
+    }
+    
+    public function offsetSet( $index,$value ){
+        if( is_scalar( $value ) ){
+            $t = gettype( $value );
+            $m = "Members of ArraySet must be scalar; [$t] provided";
+            throw new InvalidArgumentException( $m,E_USER_NOTICE);
+        }
+        parent::offsetSet( $value,$value );
+    }
+}
